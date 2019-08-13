69e7192510f0986c7b3df8ac790dbb1be49830fc
Ty Long <dtylerlong@gmail.com>
2019-08-13 12:46:42 +0000

    Upload New File

---
+++ polyRationalNum/PolyRationalNum.cpp
@@ -0,0 +1,20 @@
+// PolyRationalNum.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Polynomial.h"
+#include "RationalNum.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	Polynomial Poly;
+	
+	Poly.setTerm();
+
+	system("PAUSE");
+	return 0;
+}
+
